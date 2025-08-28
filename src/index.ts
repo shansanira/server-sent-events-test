5bc9bdc4cced93af7c0383a6e1a5f053605c893d
Sanira Liyanage <shansanira@gmail.com>
2025-08-28 23:48:12 +0200

    introduced nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3100;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express + TypeScript!');
